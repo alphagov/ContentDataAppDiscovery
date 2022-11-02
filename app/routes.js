53fb87ea2b10b2ca8e1bc4ef17b9cbfd00d58235
mgearon-gds <103048725+mgearon-gds@users.noreply.github.com>
2022-11-02 09:11:29 +0000

    Styling for multiselect filter

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,15 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.get('/multi-select-option-one', function (req, res) {
+  // Render the confirm company page
+  const documentType = require('./data/document-type.json');
+  const organisation = require('./data/organisation.json');
+
+  res.render('multi-select-option-one', {
+    // To use the company data on that page use the following
+    documentType: documentType,
+    organisation: organisation
+  })
+})
