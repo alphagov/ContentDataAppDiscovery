2f74fc0b0fb88d571c57522e2ad9b1a78047df3a
mgearon-gds <103048725+mgearon-gds@users.noreply.github.com>
2022-11-24 12:43:03 +0000

    Clean up code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,55 +7,48 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
-
-
 })
 
+//Hide the help us panel on click
 const btn = document.getElementById('hide-this');
 
 btn.addEventListener('click', () => {
   const box = document.getElementById('hide-help-us');
-
   box.style.display = 'none';
-
 });
 
+//Multi search filter search and hide values that do not match the search term
 function documentType() {
-var input, filter, ul, li, a, i, txtValue;
-input = document.getElementById('document-type-input');
-filter = input.value.toUpperCase();
-ul = document.getElementById("document-type-list");
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('document-type-input');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("document-type-list");
   li = ul.getElementsByTagName("div");
-
-// Loop through all list items, and hide those who don't match the search query
-for (i = 0; i < li.length; i++) {
-  a = li[i].getElementsByTagName("label")[0];
-  txtValue = a.textContent || a.innerText;
-  if (txtValue.toUpperCase().indexOf(filter) > -1) {
-    li[i].style.display = "";
-  } else {
-    li[i].style.display = "none";
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("label")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
   }
 }
-}
-
-
-
 
 function organisationType() {
-var input, filter, ul, li, a, i, txtValue;
-input = document.getElementById('organisation-input');
-filter = input.value.toUpperCase();
-ul = document.getElementById("organisation-list");
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('organisation-input');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("organisation-list");
   li = ul.getElementsByTagName("div");
-
-for (i = 0; i < li.length; i++) {
-  a = li[i].getElementsByTagName("label")[0];
-  txtValue = a.textContent || a.innerText;
-  if (txtValue.toUpperCase().indexOf(filter) > -1) {
-    li[i].style.display = "";
-  } else {
-    li[i].style.display = "none";
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("label")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
   }
-}
 }
--- app/routes.js
+++ app/routes.js
@@ -5,15 +5,18 @@ const router = express.Router()
 
 module.exports = router
 
+//All of these routes are being used as a way to load in data from the data folder.
+
 router.get('/multi-select', function (req, res) {
   // Render the confirm company page
   const documentType = require('./data/document-type.json');
   const organisation = require('./data/organisation.json');
-
+  const table = require('./data/table.json');
   res.render('multi-select', {
     // To use the company data on that page use the following
     documentType: documentType,
-    organisation: organisation
+    organisation: organisation,
+    table: table
   })
 })
 
@@ -21,10 +24,51 @@ router.get('/multi-select-results', function (req, res) {
   // Render the confirm company page
   const documentType = require('./data/document-type.json');
   const organisation = require('./data/organisation.json');
+  const table = require('./data/table.json');
 
   res.render('multi-select-results', {
     // To use the company data on that page use the following
     documentType: documentType,
-    organisation: organisation
+    organisation: organisation,
+    table: table
+  })
+})
+
+router.get('/alternative-view', function (req, res) {
+  // Render the confirm company page
+  const documentType = require('./data/document-type.json');
+  const organisation = require('./data/organisation.json');
+  const table = require('./data/table.json');
+  res.render('alternative-view', {
+    // To use the company data on that page use the following
+    documentType: documentType,
+    organisation: organisation,
+    table: table
+  })
+})
+
+router.get('/date-range-option-one', function (req, res) {
+  // Render the confirm company page
+  const documentType = require('./data/document-type.json');
+  const organisation = require('./data/organisation.json');
+  const table = require('./data/table.json');
+  res.render('date-range-option-one', {
+    // To use the company data on that page use the following
+    documentType: documentType,
+    organisation: organisation,
+        table: table
+  })
+})
+
+router.get('/date-range-option-two', function (req, res) {
+  // Render the confirm company page
+  const documentType = require('./data/document-type.json');
+  const organisation = require('./data/organisation.json');
+  const table = require('./data/table.json');
+  res.render('date-range-option-two', {
+    // To use the company data on that page use the following
+    documentType: documentType,
+    organisation: organisation,
+        table: table
   })
 })
